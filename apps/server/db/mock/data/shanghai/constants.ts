29205be90e55a15df672f1c06631df35d20f5d27
Ivan Kornilov <ikornilov.ext@prosebya.ru>
2026-04-09 11:07:50 +0300

    yeap yeap

--- apps/server/db/mock/data/shanghai/constants.ts
+++ apps/server/db/mock/data/shanghai/constants.ts
@@ -1,4 +1,4 @@
-import { MOCK_USER_ID_1, MOCK_USER_ID_2 } from 'db/mock/00.users'
+import { MOCK_USER_ID_1 } from 'db/mock/00.users'
 import {
   getMemoriesPath as getMemoriesPathFromLib,
   getRoutePath as getRoutePathFromLib,
@@ -6,7 +6,6 @@ import {
 
 export const TRIP_ID = '3e8e2c7c-7a7b-4d43-9f5b-1a2c3d4e5f6a'
 export const USER_ID_1 = MOCK_USER_ID_1
-export const USER_ID_2 = MOCK_USER_ID_2
 
 export const DAY_IDS = {
   DAY_01: crypto.randomUUID(),
